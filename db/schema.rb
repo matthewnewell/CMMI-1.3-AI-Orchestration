1c181447f90726874b0331b07b5808b91009e191
Matthew Newell <matthew.r.newell@gmail.com>
2014-07-27 17:48:49 -0400

    add d3 animation to homepage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703170452) do
+ActiveRecord::Schema.define(version: 20140703170206) do
 
   create_table "generic_goals", force: true do |t|
     t.string   "name"
@@ -64,13 +64,6 @@ ActiveRecord::Schema.define(version: 20140703170452) do
     t.string   "name"
     t.text     "description"
     t.integer  "process_area_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "specific_practice_elaborations", force: true do |t|
-    t.integer  "specific_practice_id"
-    t.text     "sp_elaboration"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
